b803e826b1482cdf32c0e7df216bf717955e8416
Mirek Skrabal <mirekskrabal94@gmail.com>
2020-09-04 10:41:09 +0200

    add rpnstack to rpnvec

--- workswell_calc/exprparser.cpp
+++ workswell_calc/exprparser.cpp
@@ -2,16 +2,18 @@
 
 
 
-QString exprParser::calculate(QString expr)
+QString ExprParser::calculate(QString expr)
 {
     strToArr(expr.toStdString());
     tokenize();
     infToPostf();
     charArr.clear(); //clear for next
-    return expr;
+    tokenized.clear();
+    return evaluatePostfExpr();
 }
 
-void exprParser::tokenize() {
+void ExprParser::tokenize()
+{
     std::string word;
     std::string num;
     std::string tmp;
@@ -38,7 +40,8 @@ void exprParser::tokenize() {
     }
 }
 
-void exprParser::strToArr(std::string &&raw){
+void ExprParser::strToArr(std::string &&raw)
+{
     for (auto i = raw.begin(); i != raw.end(); ++i) {
         if (!isspace(*i)){
             charArr.push_back(*i);
@@ -46,11 +49,12 @@ void exprParser::strToArr(std::string &&raw){
     }
 }
 
-void exprParser::infToPostf() {
+void ExprParser::infToPostf()
+{
     std::stack<std::string> tmp;
     for (auto &i : tokenized) {
         if (isdigit(i[0])) {
-            rpnOutput.push(i);
+            rpnStack.push(i);
         }
         else if (i == "sin" || i == "cos" || i == "tg" || i == "cotg" ||
             i == "exp" || i == "log" || i[0] == '-') {
@@ -58,16 +62,16 @@ void exprParser::infToPostf() {
         }
         else if (i[0] != '(' && i[0] != ')'){
             if (i[0] == '*' || i[0] == '/') {
-                while (!tmp.empty() && (tmp.top() == "+" || tmp.top() == "-" ||
-                       tmp.top() == "*" || tmp.top() == "/")) {
-                    rpnOutput.push(tmp.top());
+                while (!tmp.empty() && (tmp.top() == "*" || tmp.top() == "/")) {
+                    rpnStack.push(tmp.top());
                     tmp.pop();
                 }
                 tmp.push(i);
             }
             else if (i[0] == '+' || i[0] == '-') {
-                while (!tmp.empty() && (tmp.top() == "+" || tmp.top() == "-" )) {
-                    rpnOutput.push(tmp.top());
+                while (!tmp.empty() && (tmp.top() == "+" || tmp.top() == "-" ||
+                                        tmp.top() == "*" || tmp.top() == "/")) {
+                    rpnStack.push(tmp.top());
                     tmp.pop();
                 }
                 tmp.push(i);
@@ -79,7 +83,7 @@ void exprParser::infToPostf() {
             }
             else {
                 while (!tmp.empty() && tmp.top() != "(") {
-                    rpnOutput.push(tmp.top());
+                    rpnStack.push(tmp.top());
                     tmp.pop();
                 }
                 tmp.pop();
@@ -88,14 +92,32 @@ void exprParser::infToPostf() {
                     auto x = tmp.top();
                 if (x ==  "sin" || x == "cos" || x == "tg" || x == "cotg" ||
                     x == "exp" || x == "log") {
-                    rpnOutput.push(tmp.top());
+                    rpnStack.push(tmp.top());
                     tmp.pop();
                 }
             }
         }
     }
     while (!tmp.empty()) {
-        rpnOutput.push(tmp.top());
+        rpnStack.push(tmp.top());
+        tmp.pop();
+    }
+}
+
+QString ExprParser::evaluatePostfExpr()
+{
+
+}
+
+void ExprParser::stackToVec()
+{
+    std::stack<std::string> tmp;
+    while (!rpnStack.empty()){
+        tmp.push(move(rpnStack.top()));
+        rpnStack.pop();
+    }
+    while (!tmp.empty()){
+        rpnVec.push_back(move(tmp.top()));
         tmp.pop();
     }
 }
